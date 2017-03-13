bfd9e96b919b902162cb1c08bdbd8a4f1229fd48
Cat Cai <catherine.cai@grindr.com>
2017-03-12 21:50:18 -0700

    Firm up implementation.

--- airbnb/add_two_numbers/main.go
+++ airbnb/add_two_numbers/main.go
@@ -7,7 +7,7 @@ Algorithm/pseudocode:
  The inputs are reversed, so if input is (2 -> 4 -> 3) and (5 -> 6 -> 4), then add the ones place (2 + 5). If this value is greater than 10, you'd have to carry that value over to the next, which you have to do with 4+6 = 10. Since it's 10, you'd leave the 0 and carry the 1 over to the next sum (4 + 3) + 1 = 8. So the answer is 807, which you'd reverse and return 7 -> 0 -> 8.
 */
 func addTwoNumbers(l1, l2 *list.List) *list.List {
-	var sum int
+	var carry, sum int
 
 	sumList := list.New()
 
@@ -16,9 +16,7 @@ func addTwoNumbers(l1, l2 *list.List) *list.List {
 	current2 := l2.Front()
 
 	for current1 != nil || current2 != nil {
-		// We want to divide the sum by 10 because, for example, if the value carried over from the previous iteration is 10, then we'll want to add 1 to the sum of this next node
-		sum /= 10
-
+		sum = carry
 		if current1 != nil {
 			sum += current1.Value.(int)
 			current1 = current1.Next()
@@ -29,6 +27,13 @@ func addTwoNumbers(l1, l2 *list.List) *list.List {
 		}
 		// We want the modulo
 		sumList.PushBack(sum % 10)
+
+		// We're going to carry over the tens
+		if sum >= 10 {
+			carry = 1
+		} else {
+			carry = 0
+		}
 	}
 	return sumList
 }
